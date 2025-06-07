f93e2715155fb1d7c008b5b3739470d47b3c9bd2
hiqatech <hiqatech@gmail.com>
2025-06-07 12:13:47 +0200

    update

--- cypress/e2e/bankingproject_depositcheck.cy.ts
+++ cypress/e2e/bankingproject_depositcheck.cy.ts
@@ -12,20 +12,12 @@ describe('Banking project', () => {
     cy.contains('Deposit Successful').should('be.visible')
     cy.get('.borderM > :nth-child(3) > :nth-child(2)').should('have.text', '100')
     cy.wait(1000)
-    cy.contains('Transactions').click()
-    cy.get('#anchor0 > :nth-child(2)').should('have.text', '100')
-    cy.contains('Back').click()
-    cy.contains('Deposit').click()
-
+ 
     cy.get('[placeholder="amount"]').type('10')
     cy.get('[type="submit"]').click()
     cy.contains('Deposit Successful').should('be.visible')
     cy.get('.borderM > :nth-child(3) > :nth-child(2)').should('have.text', '110')
     cy.wait(1000)
-    cy.contains('Transactions').click()
-    cy.get('#anchor1 > :nth-child(2)').should('have.text', '10')
-    cy.contains('Back').click()
-    cy.contains('Deposit').click()
 
     cy.get('[placeholder="amount"]').type('5')
     cy.get('[type="submit"]').click()
@@ -33,6 +25,8 @@ describe('Banking project', () => {
     cy.get('.borderM > :nth-child(3) > :nth-child(2)').should('have.text', '115')
     cy.wait(1000)
     cy.contains('Transactions').click()
+    cy.get('#anchor0 > :nth-child(2)').should('have.text', '100')
+    cy.get('#anchor1 > :nth-child(2)').should('have.text', '10')
     cy.get('#anchor2 > :nth-child(2)').should('have.text', '5')
     cy.contains('Back').click()
 
