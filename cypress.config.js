54edfb25087096d7553d00a3dd044d7a6556edcc
hiqatech <hiqatech@gmail.com>
2025-09-26 20:52:26 +0200

    fix main url

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-    },
-    baseUrl: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login'
+    baseUrl: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login',
   },
 })
--- cypress/e2e/banking/modules/login_page.js
+++ cypress/e2e/banking/modules/login_page.js
@@ -7,7 +7,7 @@ export class LoginPage {
     main_url = 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login'
 
     clickCustomerLogin() {
-        cy.visit(main_url)
+        cy.visit(this.main_url)
         cy.get(this.customer_login).click()
     }
 
